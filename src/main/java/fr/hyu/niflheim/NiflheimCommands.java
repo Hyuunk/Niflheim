5ba45489c7dbbc46e778f0f9064e72a9ac9e0b7d
Mathéo <65279654+Skitroz@users.noreply.github.com>
2023-10-24 23:30:04 +0200

    update spawn

--- src/main/java/fr/hyu/niflheim/NiflheimCommands.java
+++ src/main/java/fr/hyu/niflheim/NiflheimCommands.java
@@ -22,26 +22,27 @@ public class NiflheimCommands implements CommandExecutor {
 
             switch (cmd.getLabel().toUpperCase()) {
                 case "GMA": {
-                    setGameMode(GameMode.ADVENTURE, args, player);
-                    break;
+                    //if (!Player.hasPermission) break;
+                    setGameMode(GameMode.ADVENTURE, args, player); break;
                 }
                 case "GMC": {
-                    setGameMode(GameMode.CREATIVE, args, player);
-                    break;
+                    //if (!Player.hasPermission) break;
+                    setGameMode(GameMode.CREATIVE, args, player); break;
                 }
                 case "GMS": {
-                    setGameMode(GameMode.SURVIVAL, args, player);
-                    break;
+                    //if (!Player.hasPermission) break;
+                    setGameMode(GameMode.SURVIVAL, args, player); break;
                 }
                 case "GMSP": {
-                    setGameMode(GameMode.SPECTATOR, args, player);
-                    break;
+                    //if (!Player.hasPermission) break;
+                    setGameMode(GameMode.SPECTATOR, args, player); break;
                 }
                 case "SPAWN": {
                     handleSpawnCommand(player);
                     break;
                 }
                 case "SETSPAWN": {
+                    if (!player.isOp()) break;
                     handleSetSpawnCommand(player);
                     break;
                 }
@@ -51,83 +52,62 @@ public class NiflheimCommands implements CommandExecutor {
     }
 
     public void setGameMode(final GameMode gamemode, final String[] args, final Player player) {
+        if (!player.isOp()) return;
         switch (Toolskit.checkArgs(player, Toolskit.CommandsType.PLAYER, args)) {
             case 0: {
                 final Player targetPlayer = Bukkit.getPlayer(args[0]);
                 targetPlayer.setGameMode(gamemode);
                 targetPlayer.playSound(targetPlayer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 10.0f, 1.0f);
-                player.sendMessage(ChatColor.GREEN + "Tu viens de passer en mode " + gamemode.name().toLowerCase() + " " + ChatColor.GRAY + targetPlayer.getName() + ".");
-                targetPlayer.sendMessage(ChatColor.GREEN + "Ton gamemode a été changé en " + gamemode.name().toLowerCase() + ChatColor.GRAY + ".");
+                player.sendMessage(String.valueOf("ChatManager.MessageType.OLYMPCLASSIC.getMessage())" + "Tu viens de passer en mode " + ChatColor.GREEN + gamemode.name().toLowerCase() + " " + ChatColor.GRAY + targetPlayer.getName() + "."));
+                targetPlayer.sendMessage(String.valueOf("ChatManager.MessageType.OLYMPCLASSIC.getMessage())" + "Ton gamemode a \u00e9t\u00e9 chang\u00e9 en " + ChatColor.GREEN + gamemode.name().toLowerCase() + ChatColor.GRAY + "."));
                 break;
             }
             case 1: {
                 player.setGameMode(gamemode);
                 player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 10.0f, 1.0f);
-                player.sendMessage(ChatColor.GREEN + "Tu viens de passer en mode " + gamemode.name().toLowerCase() + ChatColor.GRAY + ".");
+                player.sendMessage(String.valueOf("ChatManager.MessageType.OLYMPCLASSIC.getMessage())" + "Tu viens de passer en mode " + ChatColor.GREEN + gamemode.name().toLowerCase() + ChatColor.GRAY + "."));
                 break;
             }
             case -3: {
-                player.sendMessage(ChatColor.RED + "Invalid Player. Try /gm(a,c,s,sp) [<targetPlayer>]." + ChatColor.GRAY + ".");
+                player.sendMessage(String.valueOf("ChatManager.MessageType.OLYMPERROR.getMessage())" + "Invalid Player. Try /gm(a,c,s,sp)" + ChatColor.RED + " [<targetPlayer>]" + ChatColor.GRAY + "."));
                 break;
             }
         }
     }
-
     // Système de spawn
-    private FileConfiguration spawnConfig = null;
-    private File spawnFile = null;
-
-    private void initSpawnConfig() {
-        if (spawnFile == null) {
-            spawnFile = new File(Main.INSTANCE.getDataFolder(), "Niflheim/spawn.yml");
-        }
-        spawnConfig = YamlConfiguration.loadConfiguration(spawnFile);
-
-        if (!spawnFile.exists()) {
-            try {
-                spawnFile.createNewFile();
-            } catch (IOException e) {
-                Main.INSTANCE.getLogger().warning("Impossible de créer le fichier de configuration spawn.yml.");
-            }
-        }
-    }
-
-    private void saveSpawnConfig() {
-        if (spawnConfig == null || spawnFile == null) {
-            return;
-        }
-        try {
-            spawnConfig.save(spawnFile);
-        } catch (IOException e) {
-            Main.INSTANCE.getLogger().warning("Impossible de sauvegarder le fichier de configuration spawn.yml.");
-        }
-    }
 
     private void handleSetSpawnCommand(Player player) {
         Location spawnLocation = player.getLocation();
+        File file = new File(Main.INSTANCE.getDataFolder(), "spawn.yml");
+        FileConfiguration spawnConfig = (FileConfiguration)YamlConfiguration.loadConfiguration(file);
 
-        spawnConfig.set("spawn.world", spawnLocation.getWorld().getName());
         spawnConfig.set("spawn.x", spawnLocation.getBlockX());
         spawnConfig.set("spawn.y", spawnLocation.getBlockY());
         spawnConfig.set("spawn.z", spawnLocation.getBlockZ());
-
-        saveSpawnConfig();
+        spawnConfig.set("spawn.yaw", spawnLocation.getYaw());
+        spawnConfig.set("spawn.pitch", spawnLocation.getPitch());
+        try {
+            spawnConfig.save(file);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
         player.sendMessage(ChatColor.GREEN + "Le spawn a été défini à votre emplacement actuel.");
     }
 
     private void handleSpawnCommand(Player player) {
-        if (spawnConfig.contains("spawn")) {
-            World world = Bukkit.getWorld(spawnConfig.getString("spawn.world"));
-            int x = spawnConfig.getInt("spawn.x");
-            int y = spawnConfig.getInt("spawn.y");
-            int z = spawnConfig.getInt("spawn.z");
+        File file = new File(Main.INSTANCE.getDataFolder(), "spawn.yml");
+        FileConfiguration spawnConfig = (FileConfiguration)YamlConfiguration.loadConfiguration(file);
+        double x = spawnConfig.getDouble("spawn.x");
+        double y = spawnConfig.getDouble("spawn.y");
+        double z = spawnConfig.getDouble("spawn.z");
+        double yaw = spawnConfig.getDouble("spawn.yaw");
+        double pitch = spawnConfig.getDouble("spawn.pitch");
 
-            Location spawnLocation = new Location(world, x, y, z);
-            player.teleport(spawnLocation);
-            player.sendMessage(ChatColor.GREEN + "Vous avez été téléporté au spawn personnalisé.");
-        } else {
-            player.sendMessage(ChatColor.RED + "Le spawn personnalisé n'est pas défini.");
-        }
+        Location spawnLocation = new Location(Bukkit.getWorld("world"), x, y, z, (float) yaw, (float) pitch);
+        player.teleport(spawnLocation);
+        player.sendMessage(ChatColor.GREEN + "Vous avez été télporté au spawn");
+
     }
 }
