bba39d518d847ef7e1d387c754c2e95405f10d26
Hyuunk <98412595+Hyuunk@users.noreply.github.com>
2023-10-24 21:41:40 +0200

    preparation des EconomyCommands

--- src/main/java/fr/hyu/niflheimEconomy/EconomyCommands.java
+++ src/main/java/fr/hyu/niflheimEconomy/EconomyCommands.java
@@ -1,4 +1,13 @@
 package fr.hyu.niflheimEconomy;
 
-public class EconomyCommands {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class EconomyCommands implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String arg2, String[] args) {
+        return false;
+    }
+
 }
